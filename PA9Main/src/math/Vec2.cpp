4adb8c8e30903140c0b43a040df0cb78e527bfe1
Henry Shaw <henryshaw747@gmail.com>
2023-04-22 22:39:02 -0700

    remove old inherting Vec2f class

--- PA9Main/src/math/Vec2.cpp
+++ PA9Main/src/math/Vec2.cpp
@@ -16,12 +16,6 @@ const Vector2f Vec2::Zero = Vector2f(0.f, 0.f);
 const Vector2f Vec2::I = Vector2f(1.f, 0.f);
 const Vector2f Vec2::J = Vector2f(0.f, 1.f);
 
-std::ostream& Vec2::operator<<(std::ostream& lhs, const Vector2f& rhs)
-{
-	lhs << std::setprecision(4) << rhs.x << ", " << rhs.y;
-	return lhs;
-}
-
 float Vec2::mag(Vector2f v)
 {
 	return sqrt(v.x * v.x + v.y * v.y);
@@ -48,52 +42,8 @@ Vector2f Vec2::lcross(Vector2f v)
 	return Vector2f(-v.y, v.x);
 }
 
-
-// OLD Vec2f DERIVED CLASS (DO NOT USE) //
-
-const Vec2f Vec2f::Zero(0, 0); // define the zero static member
-
-Vec2f::Vec2f() : sf::Vector2f() {};
-
-Vec2f::Vec2f(float x, float y) : sf::Vector2f(x, y) {};
-
-Vec2f::Vec2f(const sf::Vector2f& copy)
+std::ostream& operator<<(std::ostream& lhs, const Vector2f& rhs)
 {
-	*this = copy;
-}
-
-Vec2f& Vec2f::operator= (const sf::Vector2f& lhs) {
-	x = lhs.x;
-	y = lhs.y;
-	return *this;
-}
-
-std::ostream& operator << (std::ostream& lhs, const Vec2f& rhs) {
 	lhs << std::setprecision(4) << rhs.x << ", " << rhs.y;
 	return lhs;
-}
-
-float Vec2f::mag() const
-{
-	return sqrtf(x * x + y * y);
-}
-
-Vec2f Vec2f::norm() const
-{
-	return *this / mag();
-}
-
-float Vec2f::dot(const Vec2f& lhs) const
-{
-	return x * lhs.x + y * lhs.y;
-}
-
-Vec2f Vec2f::rcross() const
-{
-	return Vec2f(y, -x);
-}
-
-Vec2f Vec2f::lcross() const
-{
-	return Vec2f(-y, x);
 }
--- PA9Main/src/math/Vec2.h
+++ PA9Main/src/math/Vec2.h
@@ -9,14 +9,9 @@
 #include <iostream>
 
 #include "SFML/System.hpp"
+using sf::Vector2f; // my poor fingies (note: this in header scope so will be usable in all including files)
 
 namespace Vec2 {
-	using sf::Vector2f; // my poor fingies
-
-	// operator: stream insertion
-	// desc: for debugging by printing vector
-	std::ostream& operator << (std::ostream& lhs, const Vector2f& rhs);
-
 	// func: mag()
 	// desc: get magnitude of the vector
 	float mag(Vector2f v);
@@ -41,44 +36,6 @@ namespace Vec2 {
 	const extern Vector2f J;
 }
 
-
-/* OLD Vec2f DERIVED CLASS (DO NOT USE) */
-
-class Vec2f : public sf::Vector2f {
-public:
-
-	const static Vec2f Zero;
-
-	// ctor: default
-	// desc: initailizes to zero vector
-	Vec2f();
-
-	// ctor: from components
-	Vec2f(float x, float y);
-	
-	// ctor: copy
-	Vec2f(const sf::Vector2f& copy);
-	
-	// operator: = 
-	// desc: necessary bcus = doesn't get inherited?
-	Vec2f& operator= (const sf::Vector2f& lhs);
-
-	// operator <<
-	// desc: print the vector
-	friend std::ostream& operator<< (std::ostream& lhs, const Vec2f& rhs);
-
-	// desc: get magnitude
-	float mag() const;
-
-	// desc: get normalized (unit magnitude)
-	Vec2f norm() const;
-
-	// desc: compute dot product
-	float dot(const Vec2f& lhs) const;
-
-	// desc: compute in-plane R3 cross products (vectors normal to the vectors)
-	Vec2f rcross() const;
-	Vec2f lcross() const;
-private:
-
-};
+// operator: stream insertion
+// desc: for debugging by printing vector
+std::ostream& operator << (std::ostream& lhs, const Vector2f& rhs);
