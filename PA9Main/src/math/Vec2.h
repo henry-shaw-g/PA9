810d95c0387028e850c2efdebef250385b3ca315
Henry Shaw <henryshaw747@gmail.com>
2023-04-22 21:35:42 -0700

    change to functional vector utilities

--- PA9Main/src/math/Vec2.h
+++ PA9Main/src/math/Vec2.h
@@ -1,12 +1,48 @@
 /*
 	file: Vec2.h
-	desc: SFML Vec2 is lacking
+	desc:
+		- SFML Vec2 is lacking
+		- inheriting the Vec2 class was getting messy, so just changed to using procedureal functions
 */
 #pragma once
 
 #include <iostream>
 
 #include "SFML/System.hpp"
+
+namespace Vec2 {
+	using sf::Vector2f; // my poor fingies
+
+	// operator: stream insertion
+	// desc: for debugging by printing vector
+	std::ostream& operator << (std::ostream& lhs, const Vector2f& rhs);
+
+	// func: mag()
+	// desc: get magnitude of the vector
+	float mag(Vector2f v);
+
+	// func: norm()
+	// desc: get the normal of the vector
+	// note: returns zero vector if input is zero vector
+	Vector2f norm(Vector2f v);
+
+	// func: dot()
+	// desc: dot product operation
+	float dot(Vector2f u, Vector2f v);
+
+	// func: cross()
+	// desc: compute in-plane R3 cross products (vectors normal to the vectors)
+	Vector2f rcross(Vector2f v);
+	Vector2f lcross(Vector2f v);
+
+	// utility constants
+	const extern Vector2f Zero;
+	const extern Vector2f I;
+	const extern Vector2f J;
+}
+
+
+/* OLD Vec2f DERIVED CLASS (DO NOT USE) */
 
 class Vec2f : public sf::Vector2f {
 public:
@@ -46,4 +82,3 @@ public:
 private:
 
 };
-
