c59f9ceeed238c6e15712f6ef3549b6874181c8e
Henry Shaw <henryshaw747@gmail.com>
2023-04-24 18:33:21 -0700

    add more math utils

---
+++ PA9Main/src/math/Misc.h
@@ -0,0 +1,25 @@
+/*
+	file: math/Misc.h
+	desc: some util functions we need, (couldn't find in the std lib anywhere)
+	todo: consider namespace?
+*/
+#pragma once
+
+namespace math {
+
+	template <typename T>
+	T max(T a, T b) {
+		return a > b ? a : b;
+	}
+
+	template <typename T>
+	T min(T a, T b) {
+		return a < b ? a : b;
+	}
+
+	template <typename T>
+	T clamp(T n, T min, T high) {
+		return math::max(math::min(n, min), max);
+	}
+	
+}
