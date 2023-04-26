a10579dafbd52f795852c73254225b0f19ccb96b
Henry Shaw <henryshaw747@gmail.com>
2023-04-25 17:39:09 -0700

    temp remove GameScene errors

--- PA9Main/src/GameScene.cpp
+++ PA9Main/src/GameScene.cpp
@@ -4,10 +4,10 @@
 
 #include "GameScene.h"
 
-GameScene::GameScene() :
-	p1Tank(10, 10, 10), p1Score (0),
-	p2Tank(100, 10, 10), p2Score (0)
-{}
+//GameScene::GameScene() :
+//	p1Tank(10, 10, 10), p1Score (0),
+//	p2Tank(100, 10, 10), p2Score (0)
+//{}
 
 void GameScene::init() {}
 
